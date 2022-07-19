12049dd12c99453fd87cc6da26b94bbd8b0b2514
KorzhovVD <svalesik@mail.ru>
2022-07-19 16:24:03 +0300

    add custom hook usePosts

--- src/components/App.js
+++ src/components/App.js
@@ -8,30 +8,21 @@ import MyInput from "./UI/input/MyInput";
 import PostForm from "./PostForm/PostForm";
 import MySelect from "./UI/select/MySelect";
 import PostFilter from "./PostFilter/PostFilter";
-import MyModal from "./UI/MyModal/MyModal";
+import MyModal from "./UI/modal/MyModal";
+import { usePosts } from "../hooks/usePosts";
 
 function App() {
   
-  const [posts, setPosts] = useState([
-    {id:1, title: 'JavaScript', description: 'JS - язык программирования'},
-    {id:2, title: 'Python', description: 'ython - язык программирования'},
-    {id:3, title: 'C++', description: 'Язык программирования'}
-  ])
+  const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({sort:'', query: ''})
   const [modal, setModal] = useState(false)
+  const sortedAndSearchPosts = usePosts(posts,filter.sort, filter.query)
   
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    } 
-    return posts;
-  }, [filter.sort, posts])
 
-  const sortedAndSearchPosts = useMemo (() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+
+
 
   const createPost = (newPost) => {
     setPosts ([...posts, newPost])
